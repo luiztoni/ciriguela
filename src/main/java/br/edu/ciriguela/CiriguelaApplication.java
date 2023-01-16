7c83910241d30e47ede44c64d6f1ea63e6319209
Luiz Toni <luiztoni@outlook.com>
2023-01-16 11:13:56 -0300

    Update info

--- src/main/java/br/edu/ciriguela/CiriguelaApplication.java
+++ src/main/java/br/edu/ciriguela/CiriguelaApplication.java
@@ -2,7 +2,8 @@ package br.edu.ciriguela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Vm arguments -Dspring.profiles.active=dev
+
+//In dev mode use Vm arguments -Dspring.profiles.active=dev
 @SpringBootApplication
 public class CiriguelaApplication {
 	public static void main(String[] args) {
